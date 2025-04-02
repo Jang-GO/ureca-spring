0033792f1ad8c22be3d2e9dfb1915eebad189106
Jang-GO <jang8195@naver.com>
2025-04-02 16:54:25 +0900

    initial commit

---
+++ SpringPhoneManager/src/test/java/mycom/springphonemanager/SpringPhoneManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package mycom.springphonemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPhoneManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
